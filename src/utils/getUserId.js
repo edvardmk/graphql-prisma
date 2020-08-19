8273e916d62c9d8f108689ec252bad8bb101c6dd
EM <eduard.munkhart@gmail.com>
2020-08-19 13:12:35 +0200

    Init commit

---
+++ src/utils/getUserId.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken'
+
+const getUserId = (request, requireAuth = true) => {
+  const header = request.request ? request.request.headers.authorization : request.connection.context.Authorization
+
+  if (header) {
+    const token = header.replace('Bearer ', '')
+    const decodedToken = jwt.verify(token, 'thisisasecret')
+    return decodedToken.userId
+  }
+
+  if (requireAuth) {
+    throw new Error('Authentication required.')
+  }
+
+  return null
+}
+
+export default getUserId
